5ee35c098de49375bc012e8b50d4a55bc5e6c358
loonsthemoons <loonsthemoons@proton.me>
2026-01-12 13:30:43 +0100

    Initial Commit

---
+++ src/client/java/dev/loons/BobLockClient.java
@@ -0,0 +1,10 @@
+package dev.loons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BobLockClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
